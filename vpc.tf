af5c37f9755189cd4d9537a88a03e74f9242e455
vasutamilselvi <anbarasanind1@gmail.com>
2025-07-18 19:56:47 +0530

    modify commit

--- vpc.tf
+++ vpc.tf
@@ -3,9 +3,10 @@ locals {
 }
 resource "aws_vpc" "vpc_terra" {
   cidr_block       = var.cidr_block_vpc
-  instance_tenancy = "default"
-
+  instance_tenancy = "default"  
+  
   tags = {
     Name = "local.VPC_name"
   }
+
 }
